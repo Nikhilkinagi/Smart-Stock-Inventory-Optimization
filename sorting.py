b831e2d1a1eb17c16e7f0ca04dd635eb37002b57
Nikhil kinagi <118501478+Nikhilkinagi@users.noreply.github.com>
2025-12-15 13:06:05 +0530

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv("E:\Smart stock optimization for retail stores\data preprocessing\smart-stock.csv")
+df = pd.read_csv("/Users/nikhil.kinagi/Downloads/smart-stock(1).csv")
 print( df)
 
 print(df.sort_values(by=df.columns[0]))
@@ -9,4 +9,5 @@ print( "sorting values in decending order:\n",df.sort_values(by='sales', ascendi
 print("Quanty of having more than 30:\n" ,df[df['Quantity in Stock'] < 30])
 
 print( df[(df['Price'] >= 100) & (df['Price'] <= 1000)])
+
 

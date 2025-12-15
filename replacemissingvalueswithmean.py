98047968fdbc90404f47dfba46cf6bce5027b67e
Nikhil kinagi <118501478+Nikhilkinagi@users.noreply.github.com>
2025-12-15 13:05:31 +0530

    Update replacemissingvalueswithmean.py

--- replacemissingvalueswithmean.py
+++ replacemissingvalueswithmean.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
  
-df = pd.read_csv("E:\Smart stock optimization for retail stores\data preprocessing\smart-stock.csv")
+df = pd.read_csv("/Users/nikhil.kinagi/Downloads/smart-stock(1).csv")
+
 print("Mean:\n",df['sales'].mean())

c60e092f75f8060c450c8db1bda5597185e34a24
Nikhil kinagi <118501478+Nikhilkinagi@users.noreply.github.com>
2025-12-15 13:04:55 +0530

    Update droping-column.py

--- droping-column.py
+++ droping-column.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv("E:\Smart stock optimization for retail stores\data preprocessing\smart-stock.csv")
+df = pd.read_csv("/Users/nikhil.kinagi/Downloads/smart-stock(1).csv")
 df = df.drop("Product Name",axis=1)
 print(df)
 sorted_df = df.sort_values(by= df.columns[2])
+
 print(sorted_df)
